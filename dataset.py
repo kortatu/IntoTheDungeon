b4f347fd8effb5e1401d94c37762ff8f15671d62
Alvaro <alvaro@epiclabs.io>
2017-03-29 20:06:57 +0200

    Dataset with training and test data

---
+++ dataset.py
@@ -0,0 +1,114 @@
+import numpy
+import scipy.io
+import numpy as np
+from tensorflow.contrib.learn.python.learn.datasets import base
+from tensorflow.python.framework import dtypes
+
+class DataSet(object):
+
+    def __init__(self,
+                 images,
+                 labels,
+                 one_hot=False,
+                 dtype=dtypes.float32,
+                 reshape=True):
+        """Construct a DataSet.
+        one_hot arg is used only if fake_data is true.  `dtype` can be either
+        `uint8` to leave the input as `[0, 255]`, or `float32` to rescale into
+        `[0, 1]`.
+        """
+        dtype = dtypes.as_dtype(dtype).base_dtype
+        if dtype not in (dtypes.uint8, dtypes.float32):
+            raise TypeError('Invalid image dtype %r, expected uint8 or float32' %
+                            dtype)
+        assert images.shape[0] == labels.shape[0], (
+            'images.shape: %s labels.shape: %s' % (images.shape, labels.shape))
+        self._num_examples = images.shape[0]
+
+        # Convert shape from [num examples, rows, columns, depth]
+        # to [num examples, rows*columns] (assuming depth == 1)
+        if reshape:
+            assert images.shape[3] == 1
+            images = images.reshape(images.shape[0],
+                                    images.shape[1] * images.shape[2])
+        if dtype == dtypes.float32:
+            # Convert from [0, 255] -> [0.0, 1.0].
+            images = images.astype(numpy.float32)
+            images = numpy.multiply(images, 1.0 / 255.0)
+        self._images = images
+        self._labels = labels
+        self._epochs_completed = 0
+        self._index_in_epoch = 0
+
+    @property
+    def images(self):
+        return self._images
+
+    @property
+    def labels(self):
+        return self._labels
+
+    @property
+    def num_examples(self):
+        return self._num_examples
+
+    @property
+    def epochs_completed(self):
+        return self._epochs_completed
+
+    def next_batch(self, batch_size, fake_data=False):
+        """Return the next `batch_size` examples from this data set."""
+        if fake_data:
+            fake_image = [1] * 784
+            if self.one_hot:
+                fake_label = [1] + [0] * 9
+            else:
+                fake_label = 0
+            return [fake_image for _ in xrange(batch_size)], [
+                fake_label for _ in xrange(batch_size)
+            ]
+        start = self._index_in_epoch
+        self._index_in_epoch += batch_size
+        if self._index_in_epoch > self._num_examples:
+            # Finished epoch
+            self._epochs_completed += 1
+            # Shuffle the data
+            perm = numpy.arange(self._num_examples)
+            numpy.random.shuffle(perm)
+            self._images = self._images[perm]
+            self._labels = self._labels[perm]
+            # Start next epoch
+            start = 0
+            self._index_in_epoch = batch_size
+            assert batch_size <= self._num_examples
+        end = self._index_in_epoch
+        return self._images[start:end], self._labels[start:end]
+
+def create_data_sets(fileName):
+    """Create the data set for Into The Dungeon"""
+    mat = scipy.io.loadmat(fileName)
+    print("Keys ", mat.keys())
+    ALLX = mat['ALLX']
+    ALLY = mat['ALLY']
+    print("Shape of ALLY", ALLY.shape)
+    ALLY = np.reshape(ALLY, ALLY.shape[0])
+    ALLY0Based = ALLY - 1
+    yLabs = np.eye(7)[ALLY0Based]  # Convert a list of num 0..6 to a list of hot position array [0 0 0 1 0 0 0]
+    totalSamples = ALLY.shape[0]
+
+    print("First value NOT shuffled", yLabs[0])
+    perm = numpy.arange(totalSamples)
+    numpy.random.shuffle(perm)
+    shuffledXs = ALLX[perm]
+    shuffledYs = yLabs[perm]
+    print("First value shuffled", shuffledYs[0])
+
+    numTrainingSamples = int(totalSamples * 0.8)
+    print("Number of training", numTrainingSamples)
+    trainXs = shuffledXs[:numTrainingSamples]
+    trainYs = shuffledYs[:numTrainingSamples]
+    print("Number of test", totalSamples - numTrainingSamples)
+    testXs = shuffledXs[numTrainingSamples:]
+    testYs = shuffledYs[numTrainingSamples:]
+    print("Shape of test", testXs.shape)
+    return trainXs, trainYs, testXs, testYs
