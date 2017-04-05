dd0ec4340e6ba88074bb25233d1c85a20e42b875
Alvaro <alvaro@epiclabs.io>
2017-04-05 19:30:20 +0200

    Added tensorboard labels and summary data

--- common/perceptron.py
+++ common/perceptron.py
@@ -6,15 +6,27 @@ def getVariables(n_input, n_classes):
     n_hidden_2 = 256           # 2nd layer number of features
     # Store layers weight & bias
     weights = {
-        'h1': tf.Variable(tf.random_normal([n_input, n_hidden_1])),
-        'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2])),
-        'out': tf.Variable(tf.random_normal([n_hidden_2, n_classes]))
+        'h1': tf.Variable(tf.random_normal([n_input, n_hidden_1]), name="w1"),
+        'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2]), name="w2"),
+        'out': tf.Variable(tf.random_normal([n_hidden_2, n_classes]), name="outW")
     }
     biases = {
-        'b1': tf.Variable(tf.random_normal([n_hidden_1])),
-        'b2': tf.Variable(tf.random_normal([n_hidden_2])),
-        'out': tf.Variable(tf.random_normal([n_classes]))
+        'b1': tf.Variable(tf.constant(0.1, shape=[n_hidden_1]), name="b1"),
+        'b2': tf.Variable(tf.constant(0.1, shape=[n_hidden_2]), name="b2"),
+        'out': tf.Variable(tf.constant(0.1, shape=[n_classes]), name="outB")
     }
+    with tf.name_scope('hidden1'):
+        tf.summary.histogram("weights", weights['h1'])
+        tf.summary.histogram("biases", biases['b1'])
+    with tf.name_scope('hidden2'):
+        tf.summary.histogram("weights", weights['h2'])
+        tf.summary.histogram("biases", biases['b2'])
+    with tf.name_scope('out'):
+        tf.summary.histogram("weights", weights['out'])
+        tf.summary.histogram("biases", biases['out'])
+
+
+
     return weights, biases
 
 # Create model
@@ -25,7 +37,13 @@ def multilayer_perceptron(x, weights, biases):
     layer_1 = tf.nn.relu(layer_1)
     # Hidden layer with RELU activation
     layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
-    layer_2 = tf.nn.relu(layer_2)
+    layer_2 = tf.nn.sigmoid(layer_2)
     # Output layer with linear activation
     out_layer = tf.matmul(layer_2, weights['out']) + biases['out']
+    with tf.name_scope('hidden1'):
+        tf.summary.histogram("activations", layer_1)
+    with tf.name_scope('hidden2'):
+        tf.summary.histogram("activations", layer_2)
+    with tf.name_scope('out'):
+        tf.summary.histogram("activations", out_layer)
     return out_layer
--- dragontrainer/image_classification.py
+++ dragontrainer/image_classification.py
@@ -28,9 +28,9 @@ learning_rate = 0.001
 miLambda = 0.04
 training_epochs = 275
 batch_size = 75
-display_step = 1
+display_step = 5
 train_accuracy_step = 5
-test_accuracy_step = 5
+test_accuracy_step = 2
 
 # Network Parameters
 n_hidden_1 = 2000          # 1st layer number of features
@@ -39,8 +39,8 @@ n_input = trainXs.shape[1]    # Images data input (img shape: 180*240)
 n_classes = trainYs.shape[1]  # total classes (1-x categories)
 
 # tf Graph input
-x = tf.placeholder("float", [None, n_input])
-y = tf.placeholder("float", [None, n_classes])
+x = tf.placeholder("float", [None, n_input], name="x")
+y = tf.placeholder("float", [None, n_classes], name="labels")
 la = tf.constant(miLambda, "float", )
 
 # Construct model
@@ -49,9 +49,16 @@ pred = perceptron.multilayer_perceptron(x, weights, biases)
 regul = la * (tf.nn.l2_loss(weights['h1']) + tf.nn.l2_loss(weights['h2']) + tf.nn.l2_loss(weights['out']))
 
 # Define loss and optimizer
-sce = tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=y)
-cost = tf.reduce_mean(sce) + regul
-optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+with tf.name_scope("xentropy"):
+    sce = tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=y)
+    mean_entropy = tf.reduce_mean(sce, name="mean_entropy")
+    tf.summary.scalar('cross_entropy', mean_entropy)
+    tf.summary.scalar('regularization', regul)
+    cost = mean_entropy + regul
+    tf.summary.scalar('cost', cost)
+
+with tf.name_scope("train"):
+    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
 
 # Initializing the variables
 init = tf.global_variables_initializer()
@@ -61,9 +68,13 @@ saver = tf.train.Saver()
 model_path = base_dir + "/latest/epicmodel.ckpt"
 with tf.Session(config=tf.ConfigProto(log_device_placement=True, gpu_options=gpu_options)) as sess:
     sess.run(init)
-
+    writer = tf.summary.FileWriter("/tmp/mapmaker/1")
+    merge_summary = tf.summary.merge_all()
+    writer.add_graph(sess.graph)
     correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+    tf.summary.scalar('accuracy', accuracy)
+
     # print("Shuffled? " , train_batch)
     print("Starting optimization")
     for epoch in range(training_epochs):
@@ -73,11 +84,19 @@ with tf.Session(config=tf.ConfigProto(log_device_placement=True, gpu_options=gpu
         for i in range(total_batch):
             batch_x, batch_y = dataset.next_batch(batch_size)
             _, c = sess.run([optimizer, cost], feed_dict={x: batch_x, y: batch_y})
-            print("Epoch:", '%04d' % (epoch + 1), "Batch:", '%04d' % (i + 1), "cost=", "{:.9f}".format(c))
+            print("Epoch:", '%04d' % (epoch + 1), "Batch:", '%02d' % (i + 1), "/", '%02d' % total_batch, "cost=", "{:.9f}".format(c))
+            s = sess.run(merge_summary, feed_dict={x: batch_x, y: batch_y})
+            summary_index = epoch * total_batch + i
+
             avg_cost += c / total_batch
-        if (epoch + 1) % display_step == 0:
-            print("Epoch:", '%04d' % (epoch + 1), "Average cost=", "{:.9f}".format(avg_cost))
+
+        print("Epoch:", '%04d' % (epoch + 1), "Average cost=", "{:.9f}".format(avg_cost))
+        if summary_index % display_step == 0:
+            print("Summary index:", summary_index)
+            writer.add_summary(s, summary_index)
         if (epoch + 1) % test_accuracy_step == 0:
+            aTestResult = sess.run(pred, feed_dict={x: testXs[0:1]})
+            print("A test result", aTestResult, "ITs label:", testYs[0:1])
             print("Epoch:", '%04d' % (epoch + 1), "Accuracy test:", accuracy.eval({x: testXs, y: testYs}))
 
     # Test model
@@ -90,8 +109,4 @@ with tf.Session(config=tf.ConfigProto(log_device_placement=True, gpu_options=gpu
 
     print("Accuracy train:", accuracy.eval({x: trainXs, y: trainYs}))
     print("Accuracy test:", accuracy.eval({x: testXs, y: testYs}))
-    prediction = tf.argmax(y, 1)
 
-    # print("Weights: ", sess.run([weights['h1'], weights['h2'], weights['out']]));
-
-    # print(prediction.eval(feed_dict={x: ALLX[0:2]}))
